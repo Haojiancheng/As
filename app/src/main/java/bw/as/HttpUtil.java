23d0254ac440b12e39b483e61ab5d8b9e4f3ea8e
naughtySuperman <15311485754@163.com>
2017-09-22 11:40:05 +0800

    6

--- app/src/main/java/bw/as/HttpUtil.java
+++ app/src/main/java/bw/as/HttpUtil.java
@@ -1,8 +1,11 @@
 package bw.as;
 
+import com.ihsanbal.logging.LoggingInterceptor;
+
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.internal.platform.Platform;
 
 /**
  * Created by Administrator on 2017/9/22.
@@ -15,8 +18,15 @@ public class HttpUtil {
 
     //网络请求OKHttp
     public static void sendOkHttpRequest(String address, Callback callback){
-        OkHttpClient client = new OkHttpClient();
-//        client.networkInterceptors().add(new LoggingInterceptor());
+        OkHttpClient client = new OkHttpClient().newBuilder().addInterceptor(new LoggingInterceptor.Builder()
+                .loggable(BuildConfig.DEBUG)
+                .setLevel(com.ihsanbal.logging.Level.BASIC)
+                .log(Platform.INFO)
+                .request("Request")
+                .response("Response")
+                .addHeader("version", BuildConfig.VERSION_NAME)
+                .build()).build();
+//        client.interceptors().add(new LoggingInterceptor());
         Request request = new Request.Builder()
                 .url(address)
                 .build();
