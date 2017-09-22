23a7204a532fe2923e88090ccb3e19a30e88e022
naughtySuperman <15311485754@163.com>
2017-09-22 10:29:14 +0800

    4

--- app/src/main/java/bw/as/fragment/ZuixinribaoFragment.java
+++ app/src/main/java/bw/as/fragment/ZuixinribaoFragment.java
@@ -1,5 +1,6 @@
 package bw.as.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,16 +9,23 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.google.gson.Gson;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.loader.ImageLoader;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import bw.as.HttpUtil;
+import bw.as.ImageUtil;
 import bw.as.R;
 import bw.as.adapter.ZuixinribaoAdapter;
 import bw.as.bean.Zuixinribao;
@@ -60,13 +68,46 @@ public class ZuixinribaoFragment extends Fragment {
                 String responseData = response.body().string();
                 Zuixinribao zuixinribao = new Gson().fromJson(responseData, Zuixinribao.class);
                 List<Zuixinribao.StoriesBean> stories = zuixinribao.getStories();
+                List<Zuixinribao.TopStoriesBean> top_stories = zuixinribao.getTop_stories();
                 final ZuixinribaoAdapter zuixinribaoAdapter = new ZuixinribaoAdapter(R.layout.zuixinribao_item,stories);
+                final ArrayList<String> imageList = new ArrayList<>();
+                final ArrayList<String> titleList = new ArrayList<>();
+                for(int i = 0;i<top_stories.size();i++){
+                    imageList.add(top_stories.get(i).getImage());
+                    titleList.add(top_stories.get(i).getTitle());
+                }
+
                 final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         zuixinRecyclerView.setLayoutManager(linearLayoutManager);
                         zuixinRecyclerView.setAdapter(zuixinribaoAdapter);
+//                        View inflate = View.inflate(getActivity(), R.layout.zuixinribao_head, null);
+                View inflate = LayoutInflater.from(getActivity()).inflate(R.layout.zuixinribao_head, null);
+                        final Banner banner = inflate.findViewById(R.id.banner);
+                        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE);
+                        //设置图片加载器
+                        banner.setImageLoader(new ImageLoader() {
+                            @Override
+                            public void displayImage(Context context, Object path, ImageView imageView) {
+                                ImageUtil.loderImage((String) path,imageView);
+                            }
+                        });
+                        //设置图片集合
+                        banner.setImages(imageList);
+                        //设置banner动画效果
+                        banner.setBannerAnimation(Transformer.DepthPage);
+                        banner.setBannerTitles(titleList);
+                        //设置自动轮播，默认为true
+                        banner.isAutoPlay(true);
+                        //设置轮播时间
+                        banner.setDelayTime(1500);
+                        //设置指示器位置（当banner模式中有指示器时）
+                        banner.setIndicatorGravity(BannerConfig.CENTER);
+                        //banner设置方法全部调用完毕时最后调用
+                        zuixinribaoAdapter.addHeaderView(inflate);
+                        banner.start();
                     }
                 });
             }
